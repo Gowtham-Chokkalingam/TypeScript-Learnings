e1622804b767e8692649431a4dbe2798b6e02a3f
Gowtham-Chokkalingam <gowthamtceian@gmail.com>
2023-05-15 21:59:40 +0530

    Type script learings

--- TSC-Intro/variablesMe.ts
+++ TSC-Intro/variablesMe.ts
@@ -7,12 +7,23 @@ console.log(greetings);
 //number
 
 let userId: number = 822033.504;
-userId.toFixed()
+userId.toFixed();
 console.log(userId);
 
 // boolean
 
 let isLogged: boolean = false;
 
+// Functions in Type Script
+
+function addTwo(num: number) {
+  return num + 2;
+}
+
+function getUpper(str: string) {
+  return str.toUpperCase();
+}
+addTwo(4);
+getUpper("gowtham");
 
 export {};
