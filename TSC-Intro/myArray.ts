7e7e4a6734a156f38d312014795af7efa3d82445
Gowtham-Chokkalingam <gowthamtceian@gmail.com>
2023-05-21 11:44:15 +0530

    Ts Arrays learnings

--- TSC-Intro/myArray.ts
+++ TSC-Intro/myArray.ts
@@ -1,7 +1,20 @@
 const superHeros: string[] = [];
 const superPower: number[] = [];
 
+// we can also define the type in another way as well
+
+const soloMovies: Array<number> = [];
+
+type User = {
+  name: string;
+  isActive: boolean;
+};
+
+const allUsers: User[] = [];
+
 superHeros.push("IronMan");
 superPower.push(4);
+
+allUsers.push({ name: "Gowtham", isActive: true });
 
 export {};
