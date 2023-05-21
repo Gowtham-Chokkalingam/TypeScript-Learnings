767d2070a32b9f31e7e42eee845e6b1ec32c702c
Gowtham-Chokkalingam <gowthamtceian@gmail.com>
2023-05-21 13:40:57 +0530

    adde enum

---
+++ TSC-Intro/myEnums.ts
@@ -0,0 +1,11 @@
+// Enum is used to give the default valuse in the type script
+
+enum SeatChoice {
+  AISLE = "aisle",
+  MIDDLE = 'middel',
+  WINDOW='window',
+}
+
+const flightSeat = SeatChoice.AISLE;
+
+export {};
